5f89ad7a3b74306058f29ffbf0ac29c8d0fc93d3
Jeffesson Gomes <jeffesson01@gmail.com>
2020-06-18 02:48:00 -0300

    :art: Creating sessioncontroller

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'd73081cdd6bb9f79a104a9dca57cac70',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.use('/restaurant/employee', UserController.store);
+routes.use('/sessions', SessionController.store);
 
 export default routes;
