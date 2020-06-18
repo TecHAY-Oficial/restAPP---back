72a81f6a0a545efbf6220f95d304be0c3bc65066
Jeffesson Gomes <jeffesson01@gmail.com>
2020-06-18 02:56:30 -0300

    :bug: Bug fixed

--- src/database/migrations/20200618045627-create-users.js
+++ src/database/migrations/20200618045627-create-users.js
@@ -20,6 +20,11 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
+      is_restaurant: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,
