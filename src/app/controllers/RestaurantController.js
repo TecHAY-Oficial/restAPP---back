e95e0a37bf76e1ceac9794fe8e868d264c56ca49
Jeffesson Gomes <jeffesson01@gmail.com>
2020-07-04 16:47:29 -0300

    Criando get para pegar informacoes do restaurante

--- src/app/controllers/RestaurantController.js
+++ src/app/controllers/RestaurantController.js
@@ -3,6 +3,15 @@ import * as Yup from 'yup';
 import Restaurant from '../models/Restaurant';
 
 class RestaurantController {
+  async index(req, res) {
+    console.log(req.query.id);
+    const a = await Restaurant.findOne({
+      where: { id: req.query.id },
+    });
+
+    return res.json(a);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
--- src/routes.js
+++ src/routes.js
@@ -7,9 +7,10 @@ import AddressController from './app/controllers/AddressController';
 
 const routes = new Router();
 
-routes.use('/restaurant', RestaurantController.store);
-routes.use('/restaurant/employee', UserController.store);
-routes.use('/sessions', SessionController.store);
-routes.use('/address', AddressController.store);
+routes.post('/restaurant', RestaurantController.store);
+routes.get('/restaurant', RestaurantController.index);
+routes.post('/restaurant/employee', UserController.store);
+routes.post('/sessions', SessionController.store);
+routes.post('/address', AddressController.store);
 
 export default routes;
